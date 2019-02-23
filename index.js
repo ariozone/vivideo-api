8c4aa2d7708841316c73886f8b88b4a8548fb36d
Ario Tamaddoni <ariozone6@gmail.com>
2019-02-22 19:54:50 -0800

    Use get method to get all genres and set the app to listen to port 3000.#1

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express = require('express')
 const app = express()
+
+const genres = [
+  {id: 1, name: 'Action'},
+  {id: 2, name: 'Comedy'},
+  {id: 3, name: 'Thriller'}
+]
+app.get('/api/genres', (req, res) => {
+  res.send(genres)
+})
+
+app.listen(3000, () => console.log(`Listening on port 3000...`))
