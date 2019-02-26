3baab0e6bd4f5e07708b7d9f03bee24fcb274235
Ario Tamaddoni <ariozone6@gmail.com>
2019-02-25 17:54:37 -0800

    Using get method to handle HTTP GET requests for a specific genre.#1

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ app.get('/api/genres', (req, res) => {
 })
 
 app.get('/api/genres/:id', (req, res) => {
-  res.send(req.params.id)
+  const genre = genres.find(g => g.id === parseInt(req.params.id))
+  !genre ? res.status(404).send('Genre with the given ID does not exist.') :
+  res.send(genre.name)
 })
-
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
