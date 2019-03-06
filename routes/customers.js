091f493e84bee57a01b77c52cad8eefe2b2706d9
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-05 16:44:31 -0800

    Add name, contact and isPrime properties to schema and Create Customer model.#3

--- routes/customers.js
+++ routes/customers.js
@@ -1,7 +1,22 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const customerSchema = new mongoose.Schema({
-  name: String
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50
+  },
+  contact: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 100
+  },
+  isPrime: {
+    type: boolean,
+    default: false
+  }
 })
