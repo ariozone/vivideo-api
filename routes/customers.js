982838e953c4f30158526081c44317dd8704acfc
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-06 16:58:16 -0800

    Imported Joi and defined a schema for validation.#3

--- routes/customers.js
+++ routes/customers.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const Joi = require("joi")
 const express = require("express")
 const router = express.Router()
 router.use(express.json())
@@ -22,23 +23,36 @@ const customerSchema = new mongoose.Schema({
   }
 })
 
-const Customer = mongoose.model('Customer', customerSchema)
+const Customer = mongoose.model("Customer", customerSchema)
 
-router.get('/', async (req, res) => {
-  const customers = await Customer.find().sort({name: 1})
+router.get("/", async (req, res) => {
+  const customers = await Customer.find().sort({ name: 1 })
   res.send(customers)
 })
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const customer = await Customer.findById(req.params.id)
-  if (!customer) return res.status(404).send('Customer with given ID does not exist!')
+  if (!customer)
+    return res.status(404).send("Customer with given ID does not exist!")
   res.send(customer)
 })
 
-router.post('/', async (req, res) => {
-  let customer = new Customer({name: req.body.name, contact: req.body.contact, isPrime: req.body.isPrime})
+router.post("/", async (req, res) => {
+  let customer = new Customer({
+    name: req.body.name,
+    contact: req.body.contact,
+    isPrime: req.body.isPrime
+  })
   customer = await customer.save()
   res.send(customer)
 })
 
+const schema = {
+  name: Joi.string()
+    .min(3)
+    .max(25)
+    .required(),
+    contact: Joi.string().min(5).max(100).required(),
+    isPrime: Joi.boolean
+}
 module.exports = router
