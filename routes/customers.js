87a76e2eadebf5a1f0791042c42e11caea80ffdc
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-06 16:40:49 -0800

    Enable parsing of JSON object in the body of post requests.#3

--- routes/customers.js
+++ routes/customers.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const express = require("express")
 const router = express.Router()
+router.use(express.json())
 
 const customerSchema = new mongoose.Schema({
   name: {
