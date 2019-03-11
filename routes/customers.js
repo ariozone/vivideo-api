af6d68c3edd58542c9c90ca3a7d530316429bf52
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-11 14:06:06 -0700

    Load Customer class and validate function in customers module using object destructuring.#4

--- routes/customers.js
+++ routes/customers.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const express = require("express")
 const router = express.Router()
+const {Customer, validate} = require('../models/customer')
 router.use(express.json())
 
 router.get("/", async (req, res) => {
