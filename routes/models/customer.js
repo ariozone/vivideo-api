96afe38329321f769caeecb085f8d600e9dee048
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-11 14:00:47 -0700

    Moved the validateCustomer function from customers module to customer model module.#4

--- routes/models/customer.js
+++ routes/models/customer.js
@@ -20,4 +20,17 @@ const customerSchema = new mongoose.Schema({
   }
 })
 
-
+function validateCustomer(customer) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .max(25)
+      .required(),
+    contact: Joi.string()
+      .min(5)
+      .max(100)
+      .required(),
+    isPrime: Joi.boolean()
+  }
+  return Joi.validate(customer, schema)
+}
