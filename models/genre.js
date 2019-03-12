acdb322e6e6a7ead64a8f5a94a0eb5df9d25eb6e
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-12 14:09:59 -0700

    Moved the validateGenre function from genres module to genre model module.#4

--- models/genre.js
+++ models/genre.js
@@ -10,3 +10,12 @@ const genreSchema = new mongoose.Schema({
 })
 
 const Genre = mongoose.model("Genre", genreSchema)
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  }
+  return Joi.validate(genre, schema)
+}
