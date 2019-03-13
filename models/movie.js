d45d52e6604826b80bd60bb563d66bb89cda777c
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-13 11:26:11 -0700

    Added title, genre numberInStock and dailyRentalRate to schema and created a Movie model.#5

--- models/movie.js
+++ models/movie.js
@@ -3,8 +3,28 @@ const mongoose = require('mongoose')
 const movieSchema = new mongoose.Schema({
   title: {
     type: String,
-    require: true,
+    required: true,
     minlength: 2,
     maxlength: 50
+  },
+  genre: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50
+  },
+  numberInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255
+  },
+  dailyRentalRate: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 100
   }
 })
+
+const Movie = mongoose.model('Movie', movieSchema)
