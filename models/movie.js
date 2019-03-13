44a3f097689546cd3464e26eacdb243dd19c9578
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-13 11:59:13 -0700

    Import Joi and define a schema for validating a movie.#5

--- models/movie.js
+++ models/movie.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const genreSchema = require('./genre')
+const Joi = require('joi')
 
 const movieSchema = new mongoose.Schema({
   title: {
@@ -24,3 +25,10 @@ const movieSchema = new mongoose.Schema({
 })
 
 const Movie = mongoose.model('Movie', movieSchema)
+
+const schema = {
+  title: Joi.string().required().min(2).max(50),
+  genre: Joi.string().required().min(3).max(50),
+  numberInStock: Joi.number().required().min(0).max(255),
+  dailyRentalRate: Joi.number().required().min(0).max(255)
+}
