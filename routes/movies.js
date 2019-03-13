8546eaa46108b71519552062541f6fd0f58f5a97
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-13 16:09:54 -0700

    Enable parsing of JSON object in the body of post requests.#5

--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,7 @@ const {Movie, validate} = require('../models/movie')
 const {Genre} = require('../models/genre')
 const express = require('express')
 const router = express.Router()
+router.use(express.json())
 
 
 router.get('/', async(req, res) => {
