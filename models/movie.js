ca7eea7ac29d2a3f7014a2865202efd7e607a7f1
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-13 11:20:49 -0700

    Import mongoose and create a schema for movies.#5

--- models/movie.js
+++ models/movie.js
@@ -1,1 +1,10 @@
-// Movie model
+const mongoose = require('mongoose')
+
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+    minlength: 2,
+    maxlength: 50
+  }
+})
