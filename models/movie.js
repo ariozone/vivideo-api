9dea6e1f8c8e4a15a8b1305c75e4550a69422de7
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-13 11:29:30 -0700

    Import genreSchema, set genre property of movieSchema to imported genreSchema.#5

--- models/movie.js
+++ models/movie.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const genreSchema = require('./genre')
 
 const movieSchema = new mongoose.Schema({
   title: {
@@ -7,12 +8,7 @@ const movieSchema = new mongoose.Schema({
     minlength: 2,
     maxlength: 50
   },
-  genre: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 50
-  },
+  genre: genreSchema,
   numberInStock: {
     type: Number,
     required: true,
