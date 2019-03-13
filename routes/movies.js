8edc4845b5904cc0039b1bdd5c130226ab3a911e
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-13 15:58:19 -0700

    Used HTTP GET method to get all movies.#5

--- routes/movies.js
+++ routes/movies.js
@@ -2,3 +2,9 @@ const {Movie, validate} = require('../models/movie')
 const {Genre} = require('../models/genre')
 const express = require('express')
 const router = express.Router()
+
+
+router.get('/', async(req, res) => {
+  const movies = await Movie.find().sort('name')
+  res.send(movies)
+})
