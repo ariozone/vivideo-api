1b3acdfc39b0b988d5d1d97ca3db5ed42f4e0f95
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-14 13:48:34 -0700

    Used HTTP PUT method to validate and handle PUT requests.#5

--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,4 @@
 const { Movie, validate } = require("../models/movie")
-const { Genre } = require("../models/genre")
 const express = require("express")
 const router = express.Router()
 router.use(express.json())
@@ -24,12 +23,30 @@ router.post("/", async (req, res) => {
   let movie = new Movie({
     title: req.body.title,
     genre: {
-      _id: req.body._id,
-      name: req.body.name
+      _id: req.body.genreId._id,
+      name: req.body.genreId.name
     },
     numberInStock: req.body.numberInStock,
     dailyRentalRate: req.body.dailyRentalRate
   })
   movie = await movie.save()
+  res.send(movie)
+})
+
+router.put('/:id', async(req, res) => {
+  const {error} = validate(req.body)
+  if (error) res.status(400).send(error.details[0].message)
+  const genre = Genre.findById(req.body.genreId)
+  if (!genre) res.status(400).send('Genre ID is invalid!')
+  let movie = await Movie.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    genre: {
+      _id: req.body.genreId._id,
+      name: req.body.genreId.name
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate
+  })
+  movie= await movie.save()
   res.send(movie)
 })
