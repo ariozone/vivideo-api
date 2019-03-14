687ad1536340bd14480479f9aa9b1f6c39a5de8c
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-14 15:08:38 -0700

    Exported router. Fixed path errors.#5

--- routes/movies.js
+++ routes/movies.js
@@ -1,4 +1,5 @@
 const { Movie, validate } = require("../models/movie")
+const {Genre} = require('../models/genre')
 const express = require("express")
 const router = express.Router()
 router.use(express.json())
@@ -18,13 +19,13 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   const { error } = validate(req.body)
   if (error) return res.status(400).send(error.details[0].message)
-  const genre = Genre.findById(req.body.genreId)
+  const genre = await Genre.findById(req.body.genreId)
   if (!genre) res.status(400).send('Genre ID is invalid!')
   let movie = new Movie({
     title: req.body.title,
     genre: {
-      _id: req.body.genreId._id,
-      name: req.body.genreId.name
+      _id: genre._id,
+      name: genre.name
     },
     numberInStock: req.body.numberInStock,
     dailyRentalRate: req.body.dailyRentalRate
@@ -36,18 +37,19 @@ router.post("/", async (req, res) => {
 router.put('/:id', async(req, res) => {
   const {error} = validate(req.body)
   if (error) res.status(400).send(error.details[0].message)
+
   const genre = Genre.findById(req.body.genreId)
   if (!genre) res.status(400).send('Genre ID is invalid!')
+  
   let movie = await Movie.findByIdAndUpdate(req.params.id, {
-    name: req.body.name,
+    title: req.body.title,
     genre: {
-      _id: req.body.genreId._id,
-      name: req.body.genreId.name
+      _id: genre._id,
+      name: genre.name
     },
     numberInStock: req.body.numberInStock,
     dailyRentalRate: req.body.dailyRentalRate
   })
-  movie= await movie.save()
   res.send(movie)
 })
 
@@ -56,3 +58,5 @@ router.delete('/:id', async(req, res) => {
   if (!movie) return res.status(404).send('Movie with the given ID does not exist!')
   res.send(movie)
 })
+
+module.exports = router
