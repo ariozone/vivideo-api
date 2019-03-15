2ad51ac51bd79c28a53f86636fb103ec417afc7f
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-15 15:37:17 -0700

    Added a custom movie schema with title and dailyRentalRate properties to rentalSchema.#6

--- models/rental.js
+++ models/rental.js
@@ -20,6 +20,23 @@ const rentalSchema = new mongoose.Schema({
         required: true
       }
     })
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+      }
+
+    })
   }
 
 })
