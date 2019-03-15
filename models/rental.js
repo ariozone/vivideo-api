c6bbb9c62dbf30cc13761dd0d400e6c18f8a7e19
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-15 13:37:49 -0700

    Added a custom customer schema with isPrime and contact properties to rentalSchema.#6

--- models/rental.js
+++ models/rental.js
@@ -1,5 +1,25 @@
 const mongoose = require('mongoose')
 
 const rentalSchema = new mongoose.Schema({
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 25
+      },
+      isPrime: {
+        type: Boolean,
+        default: false
+      },
+      contact: {
+        type: String,
+        minlength: 5,
+        maxlength: 50,
+        required: true
+      }
+    })
+  }
 
 })
