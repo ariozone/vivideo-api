2d4da3a8551f7cf028f49393f07ef5473f6aebf5
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-18 18:42:32 -0700

    Validate customer and a movie and if the movie is in stock.#6

--- routes/rentals.js
+++ routes/rentals.js
@@ -13,4 +13,13 @@ router.post('/', async(req, res) => {
   const {error} = validate(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
+  const customer = await Customer.findById(req.body.customerId)
+  if (!customer) return res.status(400).send('Invalid Customer!')
+
+  const movie = await Movie.findById(req.body.movieId)
+  if (!movie) return res.status(400).send('Invalid Movie!')
+
+  if (movie.numberInStock === 0) return res.status(400).send('Movie is not in stock')
+
+
 })
