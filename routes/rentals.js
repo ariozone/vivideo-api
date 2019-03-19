e0801ae57540321d4227c324ced83e6dab1ffa90
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-19 14:09:25 -0700

    In post handler, replace numberInStock update and save implementations with fawn tasks and call fawn run() method.#6

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,30 +1,30 @@
-const {Rental, validate} = require('../models/rental')
-const {Movie} = require('../models/movie')
-const {Customer} = require('../models/customer')
-const mongoose = require('mongoose')
-const Fawn = require('fawn')
-const express = require('express')
+const { Rental, validate } = require("../models/rental")
+const { Movie } = require("../models/movie")
+const { Customer } = require("../models/customer")
+const mongoose = require("mongoose")
+const Fawn = require("fawn")
+const express = require("express")
 const router = express.Router()
 
 Fawn.init(mongoose)
 
-
-router.get('/', async(req, res) => {
-  const rentals = await Rental.find().sort({dateOut: -1})
+router.get("/", async (req, res) => {
+  const rentals = await Rental.find().sort({ dateOut: -1 })
   res.send(rentals)
 })
 
-router.post('/', async(req, res) => {
-  const {error} = validate(req.body)
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
   const customer = await Customer.findById(req.body.customerId)
-  if (!customer) return res.status(400).send('Invalid Customer!')
+  if (!customer) return res.status(400).send("Invalid Customer!")
 
   const movie = await Movie.findById(req.body.movieId)
-  if (!movie) return res.status(400).send('Invalid Movie!')
+  if (!movie) return res.status(400).send("Invalid Movie!")
 
-  if (movie.numberInStock === 0) return res.status(400).send('Movie is not in stock!')
+  if (movie.numberInStock === 0)
+    return res.status(400).send("Movie is not in stock!")
 
   let rental = new Rental({
     customer: {
@@ -37,13 +37,12 @@ router.post('/', async(req, res) => {
       title: movie.title,
       dailyRentalRate: movie.dailyRentalRate
     }
-
   })
   const task = Fawn.Task()
 
-  rental = await rental.save()
-  movie.numberInStock--
-  movie.save()
+  task
+    .save("rentals", rental)
+    .update("movies", { _id: movie._id }, { $inc: { numberInStock: -1 } })
+    .run()
   res.send(rental)
-
 })
