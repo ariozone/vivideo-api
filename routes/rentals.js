ee59550210a2fb7f88f9c528638b0f3400034144
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-18 18:24:43 -0700

    Use post method to handle POST requests.#6

--- routes/rentals.js
+++ routes/rentals.js
@@ -8,3 +8,9 @@ router.get('/', async(req, res) => {
   const rentals = await Rental.find().sort({dateOut: -1})
   res.send(rentals)
 })
+
+router.post('/', async(req, res) => {
+  const {error} = validate(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+})
