5bb92e7ab7ee420955f0bf5e1aa6604f2f3c849c
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-18 17:55:54 -0700

    Load express module and create a router object.#6

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,4 +1,5 @@
-// rentals route handlers
 const {Rental, validate} = require('../models/rental')
 const {Movie} = require('../models/movie')
 const {Customer} = require('../models/customer')
+const express = require('express')
+const router = express.Router()
