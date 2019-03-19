d68f75af90233a6412b70213b3c670f369d3ebf0
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-18 18:02:42 -0700

    Use get method to get all rentals in sorted by dateOut in descending order.#6

--- routes/rentals.js
+++ routes/rentals.js
@@ -3,3 +3,8 @@ const {Movie} = require('../models/movie')
 const {Customer} = require('../models/customer')
 const express = require('express')
 const router = express.Router()
+
+router.get('/', async(req, res) => {
+  const rentals = await Rental.find().sort({dateOut: -1})
+  res.send(rentals)
+})
