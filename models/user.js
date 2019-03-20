6e6c41add220805fe44f098b67888ab2062b07a8
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-20 13:55:24 -0700

    Added a name, an email with unique property set to true and a password property to userSchema.#7

--- models/user.js
+++ models/user.js
@@ -1,7 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024 // To be able to hash the password later.
+  }
 })
