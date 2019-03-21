808bb1dc4ea775a61a2f18c806809317309b2d0b
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-21 12:30:29 -0700

    Installed joi-password-complexity and loaded it user model.#7

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const Joi = require('joi')
+const PasswordComplexity = require('joi-password-complexity')
 
 const userSchema = new mongoose.Schema({
   name: {
