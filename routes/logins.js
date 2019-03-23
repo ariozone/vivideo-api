90b8f4c9a327d1a5592d601f77e5e9c289fd7a24
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-22 19:09:12 -0700

    Import Joi and define a schema for validating email and password in request body.#8

--- routes/logins.js
+++ routes/logins.js
@@ -2,7 +2,24 @@ const {User} = require('../models/user')
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const Joi = require('joi')
 
 router.post('/', async(req, res) => {
 
 })
+
+const schema = {
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 1024 
+  }
+}
