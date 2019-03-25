5efdf1a8cdb670921fc6266b9b6a5a665b722d01
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-25 13:16:02 -0700

    Defined a new function and implemented input validation and error handling using joi.#8

--- routes/logins.js
+++ routes/logins.js
@@ -5,21 +5,27 @@ const mongoose = require('mongoose')
 const Joi = require('joi')
 
 router.post('/', async(req, res) => {
+  const {error} = validateLogin(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
 })
 
-const schema = {
-  email: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 255,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-    maxlength: 1024 
+function validateLogin(login){
+  const schema = {
+    email: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      maxlength: 1024
+    }
   }
+  return Joi.validate(login, schema)
+
 }
