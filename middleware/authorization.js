5bd590bb3a17627131b37ab4630f87edd28d76d2
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-28 11:27:20 -0700

    Return a status: 401 and a message to client if token is not provided.#9

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -3,4 +3,8 @@ const config = require("config")
 
 module.exports = function auth(req, res, next) {
   const token = req.header("x-auth-token")
+  if (!token)
+    return res
+      .status(401)
+      .send("User is not authorized to access! No token provided.")
 }
