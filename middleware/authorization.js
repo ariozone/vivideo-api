7357f43403c3eaedfd500586c93d13038fab5ea8
Ario Tamaddoni <ariozone6@gmail.com>
2019-03-28 11:53:17 -0700

    Add a user property to request and set its value to the decoded payload and call next().#9

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -9,7 +9,12 @@ module.exports = function auth(req, res, next) {
       .send("User is not authorized to access! No token provided.")
 
   try {
+    // Verifying token
     const decodedPayload = jwt.verify(token, config.get("jwtPrivateKey"))
+    // Adding a user property to request body so that in the future we can access the _id property from the request.
+    req.user = decodedPayload
+    // Passing control to the next middleware function
+    next()
   } catch (ex) {
     res.status(400).send("Token is not valid!")
   }
