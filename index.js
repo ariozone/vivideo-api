8fa7fcc12f71a378e268484122bed6bd77ad87e4
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-01 16:56:08 -0700

    In index.js register an error middleware after all middleware functions.#10

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.use("/api/movies", movies)
 app.use("/api/rentals", rentals)
 app.use("/api/users", users)
 app.use("/api/logins", logins)
+app.use(function(err, req, res, next) {})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
