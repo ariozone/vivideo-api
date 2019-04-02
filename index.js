6d0434396dea7cb85c572339b5dde24e36b48076
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-02 13:27:51 -0700

    To avoid try/catch blocks in every route handler, installed express-async-errors and loaded it in index.js.#10

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+require("express-async-errors")
 const config = require("config")
 const genres = require("./routes/genres")
 const customers = require("./routes/customers")
