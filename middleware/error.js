7ce0cb3fe3482d213933222c907277a64a27e7f6
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-02 17:37:29 -0700

    Enhanced the call to winston by calling its error method and passing err.message and err object.#10

--- middleware/error.js
+++ middleware/error.js
@@ -1,5 +1,5 @@
 const winston = require("winston")
 module.exports = function(err, req, res, next) {
-  winston.log("error", "Something is wrong!")
+  winston.error(err.message, err)
   res.status(500).send("Something went wrong!")
 }
