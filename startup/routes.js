075f79e199f02d9ea32cc5d8fa7958dec07eab59
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-10 15:15:13 -0700

    Move all the routers and error middleware from index.js to new module and adjust their address.#21

--- startup/routes.js
+++ startup/routes.js
@@ -1,4 +1,11 @@
 const express = require("express")
+const genres = require("../routes/genres")
+const customers = require("../routes/customers")
+const movies = require("../routes/movies")
+const rentals = require("../routes/rentals")
+const users = require("../routes/users")
+const logins = require("../routes/logins")
+const error = require("../middleware/error")
 module.exports = function(app) {
   app.use(express.json())
   app.use("/api/genres", genres)
