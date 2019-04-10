6853ca9f1d15f47e399affa10ad2231ae08f469f
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-10 14:58:59 -0700

    Move all the middleware code from index.js to the function in the new module.#21

--- index.js
+++ index.js
@@ -44,15 +44,5 @@ mongoose
   .then(() => console.log("Connected to MongoDB..."))
   .catch(error => console.error("Could not connect to MongoDB...", error))
 
-// Middlewares
-app.use(express.json())
-app.use("/api/genres", genres)
-app.use("/api/customers", customers)
-app.use("/api/movies", movies)
-app.use("/api/rentals", rentals)
-app.use("/api/users", users)
-app.use("/api/logins", logins)
-app.use(error)
-
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
--- startup/routes.js
+++ startup/routes.js
@@ -1,1 +1,10 @@
-module.exports = function() {}
+module.exports = function() {
+  app.use(express.json())
+  app.use("/api/genres", genres)
+  app.use("/api/customers", customers)
+  app.use("/api/movies", movies)
+  app.use("/api/rentals", rentals)
+  app.use("/api/users", users)
+  app.use("/api/logins", logins)
+  app.use(error)
+}
