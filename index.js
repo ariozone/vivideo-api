aa4b4c8ea25760aab29c3b8e2617dfe894f08f21
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-11 14:32:09 -0700

    Enhance error handling in config, instead of console.error() and exiting process, throw an exception so that the global error handler catch the exception, log it and terminate the process.#21

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const config = require("config")
 const mongoose = require("mongoose")
 const app = express()
 const Joi = require("joi")
--- startup/config.js
+++ startup/config.js
@@ -1,7 +1,6 @@
 const config = require("config")
 module.exports = function() {
   if (!config.get("jwtPrivateKey")) {
-    console.log("FATAL ERROR: jwtPrivateKey is not defined.")
-    process.exit(1)
+    throw new Error("FATAL ERROR: jwtPrivateKey is not defined.")
   }
 }
