6a077090005633c93184be0a29f94d27eef47756
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-12 18:08:06 -0700

    Enhance the implementation by creating and matching the payload with received token.#25

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -4,12 +4,13 @@ const config = require("config")
 const mongoose = require("mongoose")
 describe("user.generateToken", () => {
   it("should return a valid token", () => {
-    const user = new User({
+    const payload = {
       _id: new mongoose.Types.ObjectId().toHexString(),
       isAdmin: true
-    })
+    }
+    const user = new User(payload)
     const token = user.generateToken()
     const decodedPayload = jwt.verify(token, config.get("jwtPrivateKey"))
-    expect(decodedPayload).toMatchObject({ _id: user._id, isAdmin: true })
+    expect(decodedPayload).toMatchObject(payload)
   })
 })
