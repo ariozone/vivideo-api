6f0ada081b78f3b3f9d570124bf782e0178c300d
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-12 18:06:09 -0700

    Write a test to see if it generates a valid token.(_id must be objectID and hex String format)#25

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -1,5 +1,15 @@
 const { User } = require("../../../models/user")
 const jwt = require("jsonwebtoken")
+const config = require("config")
+const mongoose = require("mongoose")
 describe("user.generateToken", () => {
-  it("should return a valid token", () => {})
+  it("should return a valid token", () => {
+    const user = new User({
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true
+    })
+    const token = user.generateToken()
+    const decodedPayload = jwt.verify(token, config.get("jwtPrivateKey"))
+    expect(decodedPayload).toMatchObject({ _id: user._id, isAdmin: true })
+  })
 })
