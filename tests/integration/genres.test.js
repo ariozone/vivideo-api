7c9a5784636332e1f94b356d3fb2cdd5a99c2883
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-16 14:58:08 -0700

    Enhance the test by asserting that a specific genres exist in response body.#25

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -25,6 +25,8 @@ describe("/api/genres", () => {
       const response = await request(server).get("/api/genres")
       expect(response.status).toBe(200)
       expect(response.body.length).toBe(2)
+      expect(response.body.some(genre => genre.name === "genre1")).toBe(true)
+      expect(response.body.some(genre => genre.name === "genre1")).toBeTruthy()
     })
   })
 })
