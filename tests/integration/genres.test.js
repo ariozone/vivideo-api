06eb518720f70ab9d3db7a9928a574a92228169f
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-15 17:57:39 -0700

    To populate genres, import genre model and create test data.#25

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest")
+const { Genre } = require("../../models/genre")
 let server
 
 describe("/api/genres", () => {
@@ -10,6 +11,10 @@ describe("/api/genres", () => {
   })
   describe("genres.GET/", () => {
     it("should return all genres.", async () => {
+      const genre1 = await new Genre({ name: "genre1" })
+      const genre2 = await new Genre({ name: "genre2" })
+      await genre1.save()
+      await genre2.save()
       const response = await request(server).get("/api/genres")
       expect(response.status).toBe(200)
     })
