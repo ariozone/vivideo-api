4a8020c25bfa25842f18321654834c2bf632e323
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-18 14:49:56 -0700

    Add the Genre and User model and create jwt to send the token in post requests.#25

--- tests/integration/authorization.test.js
+++ tests/integration/authorization.test.js
@@ -1,14 +1,11 @@
 const request = require("supertest")
+const { Genre } = require("../../models/genre")
+const { User } = require("../../models/user")
+const jwt = require("jsonwebtoken")
 
 describe("authorization middleware", () => {
   beforeEach(() => {
     server = require("../../index")
   })
   afterEach(() => server.close())
-  it("should return 401 if no token is provided.", async () => {
-    const response = await request(server)
-      .post("/api/genres")
-      .send({ name: "genre1" })
-    expect(response.status).toBe(401)
-  })
 })
