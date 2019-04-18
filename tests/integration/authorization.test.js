c5c410f71646ebd60032759021f2a908b5731316
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-18 14:44:32 -0700

    Enhance the implementation to load the server before each test and close it after.#25

--- tests/integration/authorization.test.js
+++ tests/integration/authorization.test.js
@@ -1,7 +1,14 @@
 const request = require("supertest")
 
 describe("authorization middleware", () => {
+  beforeEach(() => {
+    server = require("../../index")
+  })
+  afterEach(() => server.close())
   it("should return 401 if no token is provided.", async () => {
     const response = await request(server)
+      .post("/api/genres")
+      .send({ name: "genre1" })
+    expect(response.status).toBe(401)
   })
 })
