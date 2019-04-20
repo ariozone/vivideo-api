965dfa7ab29ae9851083558e59ce31f9edd133ae
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-19 17:37:41 -0700

    Load authorization middleware, mongoose, the User model and create a JWT, a user object with _id and isAdmin properties and required mock functions.#25

--- tests/unit/middleware/authorization.test.js
+++ tests/unit/middleware/authorization.test.js
@@ -1,1 +1,18 @@
-describe("authorization middleware", () => {})
+const jwt = require("jsonwebtoken")
+const auth = require("../../../middleware/authorization")
+const mongoose = require("mongoose")
+const { User } = require("../../../models/user")
+
+describe("authorization middleware", () => {
+  it("should populate req.user with a payload of valid JWT", () => {
+    const user = { _id: mongoose.Types.ObjectId(), isAdmin: true }
+    const token = new User(user).generateToken()
+    const req = {
+      header: jest.fn().mockReturnValue(token)
+    }
+    const res = {} // We need to pass it to mock function
+    const next = jest.fn()
+
+    auth(req, res, next)
+  })
+})
