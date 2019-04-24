271d80d769e395f3cf7e6d9d9ed59a89d86d0c85
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-24 15:00:31 -0700

    Load the server, import Rental model and populate the database before each test and clean up after.#34

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,4 +1,12 @@
 const request = require("supertest")
+const { Rental } = require("../../models/rental")
 describe("Returns Api", () => {
+  let server
+  beforeEach(() => {
+    server = require("../../index")
+  })
+  afterEach(async () => {
+    await server.close()
+  })
   it("should test the returns api.", () => {})
 })
