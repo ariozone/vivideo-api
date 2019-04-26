3a044ea892e5b821b4afa294503a6c14392efdf9
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-26 15:46:52 -0700

    Import mongoose and use HTTP POST method to handle POST requests.#35

--- routes/returns.js
+++ routes/returns.js
@@ -1,3 +1,7 @@
 const { User } = require("../models/user")
 const express = require("express")
 const router = express.Router()
+const mongoose = require("mongoose")
+
+router.post("/", async (req, res) => {})
+module.exports = router
