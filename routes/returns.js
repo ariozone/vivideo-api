b5dd5dccd38a4950e181b8ddb50d7f7bc376ab47
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-29 16:33:47 -0700

    Enhance the route handler to pass the test by returning 400 if movieId is not provided.#35

--- routes/returns.js
+++ routes/returns.js
@@ -6,7 +6,8 @@ const mongoose = require("mongoose")
 router.post("/", async (req, res) => {
   if (!req.body.customerId)
     return res.status(400).send("Customer Id not provided.")
-
+  if (!req.body.movieId) return res.status(400).send("Movie Id is not valid.")
   res.status(401).send("Not authorized")
 })
+
 module.exports = router
