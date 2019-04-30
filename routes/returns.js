6b921ba447b088af76051508ad29dc12c1069f58
Ario Tamaddoni <ariozone6@gmail.com>
2019-04-30 15:27:17 -0700

    Enhance the route handler to pass the test by returning 404 if no rental exist for customer/movie.#35

--- routes/returns.js
+++ routes/returns.js
@@ -1,4 +1,5 @@
 const { User } = require("../models/user")
+const { Rental } = require("../models/rental")
 const express = require("express")
 const router = express.Router()
 const mongoose = require("mongoose")
@@ -7,6 +8,14 @@ router.post("/", async (req, res) => {
   if (!req.body.customerId)
     return res.status(400).send("Customer Id not provided.")
   if (!req.body.movieId) return res.status(400).send("Movie Id is not valid.")
+
+  const rental = await Rental.findOne({
+    "customer._id": req.body.customerId,
+    "movie._id": req.body.movieId
+  })
+  if (!rental)
+    return res.status(404).send("No rental exist for this customer/movie.")
+
   res.status(401).send("Not authorized")
 })
 
