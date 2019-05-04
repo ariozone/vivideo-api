9ae70c4f9180730c60e22da4dd6e26385646dd3a
Ario Tamaddoni <ariozone6@gmail.com>
2019-05-03 18:11:25 -0700

    Enhance the function to take an app object and call use method to install helmet and compression.#38

--- startup/production.js
+++ startup/production.js
@@ -1,6 +1,7 @@
 const helmet = require("helmet")
 const compression = require("compression")
 
-module.exports = function(req, res, next) {
-  next()
+module.exports = function(app) {
+  app.use(helmet()) // getting middleware function
+  app.use(compression())
 }
